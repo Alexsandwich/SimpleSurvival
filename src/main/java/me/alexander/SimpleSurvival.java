43e39c55f0a0e06de161e6fd0b369cacc4ea4ce1
Alexander Milanovich <amilanov@vols.utk.edu>
2020-05-14 03:27:53 -0400

    Initial commit

---
+++ src/main/java/me/alexander/SimpleSurvival.java
@@ -0,0 +1,16 @@
+package me.alexander;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class SimpleSurvival extends JavaPlugin {
+
+    public void onEnable() {
+
+    }
+
+    public void onDisable() {
+
+    }
+
+    
+}
