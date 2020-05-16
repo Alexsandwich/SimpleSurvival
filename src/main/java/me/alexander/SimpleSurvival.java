f8e0aec846437ca20ce83bdf50ae266ce61e6206
Alexander Milanovich <amilanov@vols.utk.edu>
2020-05-15 23:46:47 -0400

    Updated Files for Fly Command

--- src/main/java/me/alexander/SimpleSurvival.java
+++ src/main/java/me/alexander/SimpleSurvival.java
@@ -1,16 +1,38 @@
 package me.alexander;
 
+import me.alexander.commands.Fly;
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
+
+import java.security.Permission;
 
 public class SimpleSurvival extends JavaPlugin {
 
+    private static SimpleSurvival instance;
+    FileConfiguration config = this.getConfig();
+    PluginManager pm = Bukkit.getPluginManager();
+
+    public final String not_player = configString("Not_Player");
+    public final String flydisable = configString("Fly_Disabled");
+    public final String flyenabled= configString("Fly_Enabled");
+    public final String noPermission = configString("No_Permission");
+
     public void onEnable() {
+        Bukkit.getConsoleSender().sendMessage(ChatColor.GREEN + "Simple Survival Version: " + getDescription().getVersion() + " has been enabled");
+        this.getCommand("fly").setExecutor(new Fly(this));
+
+        config.options().copyDefaults(true);
+        saveConfig();
 
     }
 
     public void onDisable() {
-
+        Bukkit.getConsoleSender().sendMessage(ChatColor.RED + "Simple Survival Version: " + getDescription().getVersion() + " has been disabled");
     }
-
-    
+    private String configString(String path) {
+        return ChatColor.translateAlternateColorCodes('&', getConfig().getString(path));
+    }
 }
---
+++ src/main/java/me/alexander/commands/Fly.java
@@ -0,0 +1,62 @@
+package me.alexander.commands;
+
+import me.alexander.SimpleSurvival;
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+import java.util.ArrayList;
+
+public class Fly implements CommandExecutor {
+
+    SimpleSurvival plugin;
+
+    public Fly(SimpleSurvival instance) {
+        plugin = instance;
+    }
+
+    private ArrayList<Player> list_flying_players = new ArrayList<>();
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+        if (!(sender instanceof Player)) {
+            sender.sendMessage(plugin.not_player);
+            return true;
+        }
+        Player player = (Player) sender;
+        if (args.length == 0) {
+            if (player.hasPermission("simplesurvival.fly")) {
+                flyMethod(player);
+            } else if (args.length == 1) {
+                // Flying other people still needs to be fixed
+                if (player.hasPermission("simplesurvival.flyother")) {
+                    Player target = Bukkit.getPlayer(args[0]);
+                    flyMethod(target);
+                }
+            } else {
+                player.sendMessage(plugin.noPermission);
+                }
+        }
+        return true;
+    }
+
+        private void flyMethod (Player player){
+                if (list_flying_players.contains(player)) {
+                    player.setFlying(false);
+                    player.setAllowFlight(false);
+                    player.sendMessage(plugin.flydisable);
+                    list_flying_players.remove(player);
+                } else if (!list_flying_players.contains(player)) {
+                    list_flying_players.add(player);
+                    player.setAllowFlight(true);
+                    player.setFlying(true);
+                    player.sendMessage(plugin.flyenabled);
+                }
+            }
+        }
+
+
+
