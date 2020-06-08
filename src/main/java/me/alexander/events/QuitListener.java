25a5e25e3e779635883fa52e6220f3eb50cf6564
Alexander Milanovich <amilanov@vols.utk.edu>
2020-06-08 00:56:29 -0400

    Folder Rename + Staffmode prep

---
+++ src/main/java/me/alexander/events/QuitListener.java
@@ -0,0 +1,28 @@
+package me.alexander.events;
+
+import me.alexander.SimpleSurvival;
+import me.clip.placeholderapi.PlaceholderAPI;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerQuitEvent;
+
+public class QuitListener implements Listener {
+
+    SimpleSurvival plugin;
+
+    public QuitListener(SimpleSurvival instance) {
+        plugin = instance;
+    }
+
+    @EventHandler(priority = EventPriority.HIGHEST)
+    public void QuitEvent(PlayerQuitEvent e) {
+        Player player = e.getPlayer();
+        String joinText = plugin.quitmsg;
+
+        joinText = PlaceholderAPI.setPlaceholders(e.getPlayer(), joinText);
+
+        e.setQuitMessage(joinText);
+    }
+}
